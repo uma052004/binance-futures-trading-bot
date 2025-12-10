ae82d5ee9e9b89e9b9beb180d725133683341d1c
Uma M <2395a32c6@gprec.ac.in>
2025-12-10 21:45:24 +0530

    Added Sum of array elements.java

---
+++ Desktop/DSA/Arrays/SumOfArray.java
@@ -0,0 +1,15 @@
+public class SumOfArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i=0;i<nums.length;i++) {
+            sum += num;
+        }
+
+        System.out.println("Sum of array elements = " + sum);
+    }
+}
